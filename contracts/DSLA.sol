3c8b5be42c0616e4fa9fca455e45ac597771af33
Matías Barrios <matias.barriosn@gmail.com>
2021-05-05 20:35:21 -0400

    feat: importing DSLA contracts in single file

--- contracts/DSLA.sol
+++ contracts/DSLA.sol
@@ -1,3 +1,4 @@
-// Entrypoint
+// Entrypoint contracts
 pragma solidity 0.6.6;
+import "@stacktical/dsla-protocol/contracts/SLARegistry.sol";
 import "@stacktical/dsla-protocol/contracts/use-cases/staking-efficiency/SEMessenger.sol";
--- contracts/NetworkAnalytics.sol
+++
@@ -1,3 +0,0 @@
-// Entrypoint
-pragma solidity 0.6.6;
-import "@stacktical/dsla-protocol/contracts/use-cases/staking-efficiency/NetworkAnalytics.sol";
