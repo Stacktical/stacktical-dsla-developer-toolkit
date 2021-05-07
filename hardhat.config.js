8a44797938b360daf7171b2c0abe93f58a04184d
Matías Barrios <matias.barriosn@gmail.com>
2021-05-06 22:55:16 -0400

    feat: deploying dsla protocol along chainlink

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,13 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 require('dotenv').config();
-// require('hardhat-docgen');
-// require('hardhat-deploy');
+require('hardhat-deploy');
 require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-etherscan');
+require('babel-polyfill');
+require('babel-register');
+
+const { networkNames } = require('./environments');
 
 module.exports = {
   networks: {
@@ -15,6 +18,16 @@ module.exports = {
       accounts: {
         mnemonic: process.env.DEVELOP_MNEMONIC,
       },
+      saveDeployments: true,
+      tags: ['LinkToken', 'Oracle'],
+    },
+    [networkNames.DEVELOP]: {
+      chainId: 1337,
+      accounts: {
+        mnemonic: process.env.DEVELOP_MNEMONIC,
+      },
+      tags: ['LinkToken', 'Oracle'],
+      url: 'http://localhost:8545',
     },
   },
   solidity: {
@@ -37,14 +50,29 @@ module.exports = {
           },
         },
       },
+      {
+        version: '0.4.11',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
     ],
   },
-  // docgen: {
-  //   path: './docs',
-  //   clear: true,
-  //   runOnCompile: true,
-  // },
+  paths: {
+    sources: './dev-env/chainlink/contracts',
+    deploy: './dev-env/chainlink/contracts-deploy',
+    deployments: './dev-env/chainlink/deployments',
+    imports: './build/contracts',
+  },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
   },
 };
--- scripts/dev-env/deployment/develop.sh
+++ scripts/dev-env/deployment/develop.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 export NODE_ENV=develop
+truffle compile
 docker-compose -f dev-env/docker-compose.develop.yaml -- down
 rm -rf dev-env/postgres/develop/db dev-env/chainlink/tempkeys dev-env/chainlink/secret
 docker-compose -f dev-env/docker-compose.develop.yaml -- up -d
-truffle deploy --reset --network develop
 truffle exec --network develop scripts/dev-env/chores/prepare-node.js
 truffle exec --network develop scripts/dev-env/chores/post-bridge.js
 truffle exec --network develop scripts/dev-env/chores/post-job.js
