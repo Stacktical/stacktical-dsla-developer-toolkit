c835b1ac679fcfb050feb4ac07db12b47ca77a2c
Matías Barrios <matias.barriosn@gmail.com>
2021-06-17 16:42:58 -0400

    feat: validating schemas before execution

--- dtk-env-validation.ts
+++ dtk-env-validation.ts
@@ -2,26 +2,22 @@ const Joi = require('joi');
 require('dotenv').config();
 
 const schema = Joi.object({
-  IPFS_URI: Joi.string().default('http://localhost:5001'),
+  IPFS_URI: Joi.string().required(),
+
   MAINNET_MNEMONIC: Joi.string().required(),
   TESTNET_MNEMONIC: Joi.string().required(),
   DEVELOP_MNEMONIC: Joi.string().required(),
 
-  MUMBAI_URI: Joi.string().default(
-    'https://matic-testnet-archive-rpc.bwarelabs.com'
-  ),
-  MUMBAI_WS_URI: Joi.string().default(
-    'wss://matic-testnet-archive-ws.bwarelabs.com'
-  ),
-  POLYGON_WS_URI: Joi.string().default(
-    'wss://matic-mainnet-full-ws.bwarelabs.com'
-  ),
-  HARMONYTESTNET_URI: Joi.string().default('https://api.s0.b.hmny.io'),
-  HARMONYTESTNET_WS_URI: Joi.string().default('wss://ws.s0.b.hmny.io'),
-  HARMONY_URI: Joi.string().default('https://api.s0.t.hmny.io'),
-  HARMONY_WS_URI: Joi.string().default('wss://ws.s0.t.hmny.io'),
-  DEVELOP_URI: Joi.string().default('http://localhost:8545'),
-  DEVELOP_WS_URI: Joi.string().default('http://localhost:8545'),
+  MUMBAI_URI: Joi.string().required(),
+  MUMBAI_WS_URI: Joi.string().required(),
+  POLYGON_URI: Joi.string().required(),
+  POLYGON_WS_URI: Joi.string().required(),
+  HARMONYTESTNET_URI: Joi.string().required(),
+  HARMONYTESTNET_WS_URI: Joi.string().required(),
+  HARMONY_URI: Joi.string().required(),
+  HARMONY_WS_URI: Joi.string().required(),
+  DEVELOP_URI: Joi.string().required(),
+  DEVELOP_WS_URI: Joi.string().required(),
   ETHEREUM_URI: Joi.string().required(),
   ETHEREUM_WS_URI: Joi.string().required(),
   KOVAN_URI: Joi.string().required(),
@@ -31,7 +27,7 @@ const schema = Joi.object({
 const { error, value } = schema.validate(process.env);
 
 if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
+  throw new Error(`.env file validation error: ${error.message}`);
 } else {
   process.env = value;
 }
