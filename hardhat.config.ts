1f9ba4267e18b0041936c61a213116f764441ea8
Matías Barrios <matias.barriosn@gmail.com>
2021-07-08 15:37:46 -0400

    feat: networks disabled

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,12 +20,12 @@ import { HardhatUserConfig } from 'hardhat/types';
 
 export const networks = [
   { name: NETWORKS.DEVELOP, enabled: true, exportable: true },
-  { name: NETWORKS.KOVAN, enabled: true, exportable: true },
-  { name: NETWORKS.MUMBAI, enabled: true, exportable: false },
-  { name: NETWORKS.HARMONYTESTNET, enabled: true, exportable: false },
-  { name: NETWORKS.ETHEREUM, enabled: true, exportable: true },
-  { name: NETWORKS.HARMONY, enabled: true, exportable: true },
-  { name: NETWORKS.POLYGON, enabled: true, exportable: false },
+  { name: NETWORKS.KOVAN, enabled: false, exportable: true },
+  { name: NETWORKS.MUMBAI, enabled: false, exportable: false },
+  { name: NETWORKS.HARMONYTESTNET, enabled: false, exportable: false },
+  { name: NETWORKS.ETHEREUM, enabled: false, exportable: true },
+  { name: NETWORKS.HARMONY, enabled: false, exportable: true },
+  { name: NETWORKS.POLYGON, enabled: false, exportable: false },
 ];
 
 const getEnabledNetworks = () =>
