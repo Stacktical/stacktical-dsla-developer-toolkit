85845999cd26b1593ccccd72c6623fc2d8550459
Matías Barrios <matias.barriosn@gmail.com>
2021-07-08 18:57:12 -0400

    feat: public ipfs gateway in external adapter

--- services/external-adapter/env-validation.ts
+++ services/external-adapter/env-validation.ts
@@ -3,20 +3,27 @@ import hardhat from 'hardhat';
 const Joi = require('joi');
 require('dotenv').config();
 
-const networksObject = Object.keys(hardhat.config.networks)
-  .filter((network) => !['hardhat', 'localhost'].includes(network))
-  .reduce(
-    (r, network) => ({
-      ...r,
-      [`${network.toUpperCase()}_URI`]: Joi.string().uri().required(),
-    }),
-    {}
-  );
+// const networksObject = Object.keys(hardhat.config.networks)
+//   .filter((network) => !['hardhat', 'localhost'].includes(network))
+//   .reduce(
+//     (r, network) => ({
+//       ...r,
+//       [`${network.toUpperCase()}_URI`]: Joi.string().uri().required(),
+//     }),
+//     {}
+//   );
 
-const schema = Joi.object({
-  IPFS_URI: Joi.string().uri().required(),
-  ...networksObject,
-}).unknown();
+const schema = Joi.object(
+  Object.keys(hardhat.config.networks)
+    .filter((network) => !['hardhat', 'localhost'].includes(network))
+    .reduce(
+      (r, network) => ({
+        ...r,
+        [`${network.toUpperCase()}_URI`]: Joi.string().uri().required(),
+      }),
+      {}
+    )
+).unknown();
 
 const { error } = schema.validate(process.env);
 
