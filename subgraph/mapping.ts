843cdf047a9effcb7464b853c6f0a3baacf16781
Matías Barrios <matias.barriosn@gmail.com>
2021-07-23 00:49:03 -0400

    feat: user balances subgraph

--- subgraph/mapping.ts
+++ subgraph/mapping.ts
@@ -1,5 +1,16 @@
-import { SLA as SLATemplate } from './generated/templates';
-import { Deposit, DToken, SLA, SLI, Withdrawal } from './generated/schema';
+import {
+  SLA as SLATemplate,
+  DToken as DTokenTemplate,
+} from './generated/templates';
+import {
+  Deposit,
+  DToken,
+  DtokenBalance,
+  SLA,
+  SLI,
+  User,
+  Withdrawal,
+} from './generated/schema';
 
 import {
   Stake,
@@ -13,6 +24,15 @@ import { SLA as SLAContract } from './generated/SLARegistry/SLA';
 
 import { SLORegistered } from './generated/SLORegistry/SLORegistry';
 import { ERC20 } from './generated/templates/SLA/ERC20';
+import { Approval, Transfer } from './generated/templates/DToken/DToken';
+
+import {
+  BigDecimal,
+  BigInt,
+  ByteArray,
+  log,
+  crypto,
+} from '@graphprotocol/graph-ts';
 
 export function handleNewSLA(event: SLACreated): void {
   let slaContract = SLAContract.bind(event.params.sla);
@@ -36,6 +56,12 @@ export function handleNewSLA(event: SLACreated): void {
   sla.leverage = slaContract.leverage();
   sla.save();
   SLATemplate.create(event.params.sla);
+  let user = User.load(event.params.owner.toHexString());
+  if (!user) {
+    user = new User(event.params.owner.toHexString());
+  }
+  user.slas = user.slas.concat([sla.id]);
+  user.save();
 }
 
 export function handleSLICreated(event: SLICreated): void {
@@ -45,6 +71,7 @@ export function handleSLICreated(event: SLICreated): void {
     sla.address.toHexString() + '-' + event.params.periodId.toString();
   let sli = new SLI(sliID);
   sla.SLIs = sla.SLIs.concat([sli.id]);
+  sla.nextVerifiablePeriod = slaContract.nextVerifiablePeriod();
   sla.save();
 
   sli.periodId = event.params.periodId;
@@ -69,6 +96,12 @@ export function handleStake(event: Stake): void {
   deposit.save();
   sla.deposits = sla.deposits.concat([deposit.id]);
   sla.save();
+  let user = User.load(event.params.caller.toHexString());
+  if (!user) {
+    user = new User(event.params.caller.toHexString());
+  }
+  user.deposits = user.deposits.concat([deposit.id]);
+  user.save();
 }
 
 export function handleProviderWithdraw(event: ProviderWithdraw): void {
@@ -82,6 +115,12 @@ export function handleProviderWithdraw(event: ProviderWithdraw): void {
   withdrawal.save();
   sla.withdrawals = sla.withdrawals.concat([withdrawal.id]);
   sla.save();
+  let user = User.load(event.params.caller.toHexString());
+  if (!user) {
+    user = new User(event.params.caller.toHexString());
+  }
+  user.withdrawals = user.withdrawals.concat([withdrawal.id]);
+  user.save();
 }
 
 export function handleUserWithdraw(event: ProviderWithdraw): void {
@@ -95,6 +134,12 @@ export function handleUserWithdraw(event: ProviderWithdraw): void {
   withdrawal.save();
   sla.withdrawals = sla.withdrawals.concat([withdrawal.id]);
   sla.save();
+  let user = User.load(event.params.caller.toHexString());
+  if (!user) {
+    user = new User(event.params.caller.toHexString());
+  }
+  user.withdrawals = user.withdrawals.concat([withdrawal.id]);
+  user.save();
 }
 
 export function handleSLORegistered(event: SLORegistered): void {
@@ -118,6 +163,7 @@ export function handleDTokensCreated(event: DTokensCreated): void {
   spToken.symbol = spTokenContract.symbol();
   spToken.slaAddress = event.address;
   spToken.tokenAddress = event.params.tokenAddress;
+  spToken.totalSupply = BigInt.fromI32(0);
   sla.dTokens = sla.dTokens.concat([spToken.id]);
 
   // LP token
@@ -128,9 +174,56 @@ export function handleDTokensCreated(event: DTokensCreated): void {
   lpToken.address = event.params.dpTokenAddress;
   lpToken.symbol = lpTokenContract.symbol();
   lpToken.slaAddress = event.address;
+  lpToken.totalSupply = BigInt.fromI32(0);
   lpToken.tokenAddress = event.params.tokenAddress;
+
   sla.dTokens = sla.dTokens.concat([lpToken.id]);
+
   spToken.save();
   lpToken.save();
   sla.save();
+  DTokenTemplate.create(event.params.duTokenAddress);
+  DTokenTemplate.create(event.params.dpTokenAddress);
 }
+
+export function handleDTokenTransfer(event: Transfer): void {
+  let dToken = DToken.load(event.address.toHexString());
+  let dTokenContract = ERC20.bind(event.address);
+  dToken.totalSupply = dTokenContract.totalSupply();
+  // if from is 0, then is mint and corresponds the 'to' address
+  let userAddress =
+    event.params.from.toI32() !== 0 ? event.params.from : event.params.to;
+  let user = User.load(userAddress.toHexString());
+  if (!user) {
+    user = new User(userAddress.toHexString());
+  }
+  let dTokenBalance = DtokenBalance.load(
+    userAddress.toHexString() + event.address.toHexString()
+  );
+  if (!dTokenBalance) {
+    dTokenBalance = new DtokenBalance(
+      userAddress.toHexString() + event.address.toHexString()
+    );
+  }
+  dTokenBalance.dtoken = dToken.id;
+  dTokenBalance.slaAllowance = dTokenContract.allowance(
+    userAddress,
+    event.transaction.from
+  );
+  dTokenBalance.balance = dTokenContract.balanceOf(userAddress);
+  dToken.save();
+  dTokenBalance.save();
+
+  user.dTokensBalances = user.dTokensBalances.concat([dTokenBalance.id]);
+  user.save();
+}
+
+// export function handleDTokenApproval(event: Approval): void {
+//   let sla = SLA.load(event.params.sla.toHexString());
+//   if (!sla) {
+//     sla = new SLA(event.params.sla.toHexString());
+//   }
+//   sla.sloType = event.params.sloType;
+//   sla.sloValue = event.params.sloValue;
+//   sla.save();
+// }
