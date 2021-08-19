5a518ec1275b720e97dc45b95d84c5d91251f427
Matías Barrios <matias.barriosn@gmail.com>
2021-08-19 19:37:19 -0400

    feat: harmonytestnet exported data

--- exported-data/index.ts
+++ exported-data/index.ts
@@ -1,5 +1,6 @@
 export { develop } from './develop';
 export { ethereum } from './ethereum';
 export { harmony } from './harmony';
+export { harmonytestnet } from './harmonytestnet';
 export { polygon } from './polygon';
 export { rinkeby } from './rinkeby';
