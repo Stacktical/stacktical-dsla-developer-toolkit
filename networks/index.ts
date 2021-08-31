7ca879f0d2e641e5247f0b763c26a29dc752484b
bussyjd <bussyjd@users.noreply.github.coM>
2021-08-31 17:17:52 +0200

    chore: Post deployment artifacts

--- networks/index.ts
+++ networks/index.ts
@@ -7,7 +7,7 @@ export const networks = [
   { name: NETWORKS.HARMONYTESTNET, enabled: false, exportable: false },
   { name: NETWORKS.ETHEREUM, enabled: false, exportable: true },
   { name: NETWORKS.HARMONY, enabled: false, exportable: true },
-  { name: NETWORKS.POLYGON, enabled: true, exportable: false },
+  { name: NETWORKS.POLYGON, enabled: false, exportable: false },
   { name: NETWORKS.RINKEBY, enabled: false, exportable: true },
-  { name: NETWORKS.BSC, enabled: false, exportable: true },
+  { name: NETWORKS.BSC, enabled: true, exportable: true },
 ];
