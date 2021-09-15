ed39c4b7dba7702a7f8382d5f70e64d6ab06483a
Matías Barrios <matias.barriosn@gmail.com>
2021-09-15 12:23:03 -0300

    fix: exporting tokens without .json

--- exported-data/index.ts
+++ exported-data/index.ts
@@ -1,8 +1,8 @@
+export { avalanche } from './avalanche';
+export { bsc } from './bsc';
 export { develop } from './develop';
 export { ethereum } from './ethereum';
 export { harmony } from './harmony';
 export { harmonytestnet } from './harmonytestnet';
 export { polygon } from './polygon';
 export { rinkeby } from './rinkeby';
-export { avalanche } from './avalanche';
-export { bsc } from './bsc';
--- networks/index.ts
+++ networks/index.ts
@@ -1,14 +1,14 @@
 import { NETWORKS } from '../constants';
 
 export const networks = [
-  { name: NETWORKS.DEVELOP, enabled: false, exportable: true },
-  { name: NETWORKS.KOVAN, enabled: false, exportable: true },
-  { name: NETWORKS.MUMBAI, enabled: false, exportable: false },
-  { name: NETWORKS.HARMONYTESTNET, enabled: false, exportable: false },
-  { name: NETWORKS.ETHEREUM, enabled: false, exportable: true },
-  { name: NETWORKS.HARMONY, enabled: false, exportable: true },
-  { name: NETWORKS.POLYGON, enabled: false, exportable: false },
-  { name: NETWORKS.RINKEBY, enabled: false, exportable: true },
-  { name: NETWORKS.BSC, enabled: true, exportable: true },
-  { name: NETWORKS.AVALANCHE, enabled: true, exportable: true },
+  { name: NETWORKS.DEVELOP, enabled: true },
+  { name: NETWORKS.KOVAN, enabled: true },
+  { name: NETWORKS.MUMBAI, enabled: true },
+  { name: NETWORKS.HARMONYTESTNET, enabled: true },
+  { name: NETWORKS.ETHEREUM, enabled: true },
+  { name: NETWORKS.HARMONY, enabled: true },
+  { name: NETWORKS.POLYGON, enabled: true },
+  { name: NETWORKS.RINKEBY, enabled: true },
+  { name: NETWORKS.BSC, enabled: true },
+  { name: NETWORKS.AVALANCHE, enabled: true },
 ];
