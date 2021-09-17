b40b86f20905b49add3c75fec101d98dd84bfd23
Matías Barrios <matias.barriosn@gmail.com>
2021-09-17 13:50:28 -0300

    feat: ganache with 2s blocktime

--- exported-data/index.ts
+++ exported-data/index.ts
@@ -1,8 +1,1 @@
-export { avalanche } from './avalanche';
-export { bsc } from './bsc';
 export { develop } from './develop';
-export { ethereum } from './ethereum';
-export { harmony } from './harmony';
-export { harmonytestnet } from './harmonytestnet';
-export { polygon } from './polygon';
-export { rinkeby } from './rinkeby';
