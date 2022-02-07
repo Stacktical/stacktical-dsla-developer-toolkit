16766d509d0709bc5add59281ab4a8a9c6f94024
Luca Carlig <luca.carlig@stacktical.com>
2022-02-07 18:13:24 +0000

    clean up

--- subgraph/health.ts
+++ subgraph/health.ts
@@ -1,7 +1,10 @@
 const axios = require('axios');
 
 const PLAYGROUND_URL = 'http://127.0.0.1:8030/graphql';
+
+// INSERT YOUR OWN DEPLOYMENT ID
 const DEPLOYMENT_ID = 'QmNTkuTujyCXe3FBAm8YoVwfQaSwcR9AkqCmL8CnAkv7FA';
+
 axios({
   url: PLAYGROUND_URL,
   method: 'post',
