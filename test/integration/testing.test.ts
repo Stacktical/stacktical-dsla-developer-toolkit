5be64d62e19eddc67680cde485c38adc3dc474fb
gmtesla <g.topman09@gmail.com>
2022-07-30 00:19:24 +1000

    fix: fix lp & sp symbol unit test

--- test/integration/testing.test.ts
+++ test/integration/testing.test.ts
@@ -36,8 +36,10 @@ describe('Fixtures', () => {
   describe("check SP & LP token names of messenger contracts", async () => {
     it(CONTRACT_NAMES.BaseOracle, async () => {
       const base_messenger: BaseOracle = await ethers.getContract(CONTRACT_NAMES.BaseOracle);
-      expect(await base_messenger.lpName()).to.be.eq(SERVICE_CREDITS.STAKING_REWARDS.DSLA_LP.name);
-      expect(await base_messenger.spName()).to.be.eq(SERVICE_CREDITS.STAKING_REWARDS.DSLA_SP.name);
+      expect(await base_messenger.lpName()).to.be.eq("Dummy Service Credit");
+      expect(await base_messenger.spName()).to.be.eq("Dummy Service Credit");
+      expect(await base_messenger.lpSymbol()).to.be.eq('🔺FOO');
+      expect(await base_messenger.spSymbol()).to.be.eq('🔻BAR');
     })
     it(CONTRACT_NAMES.InflationOracle, async () => {
       const inflation_messenger: InflationOracle = await ethers.getContract(CONTRACT_NAMES.InflationOracle);
