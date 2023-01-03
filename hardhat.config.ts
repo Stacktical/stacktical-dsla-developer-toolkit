5dd688e47c6828611b90dcae4fabc7c1f3da7d97
Wilhem Pujar <wilhem.pujar@stacktical.com>
2023-01-03 12:37:45 +0100

    fix: pragma

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -43,6 +43,15 @@ const config: HardhatUserConfig = {
         },
       },
       {
+        version: '^0.7.0',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
         version: '0.6.6',
         settings: {
           optimizer: {
